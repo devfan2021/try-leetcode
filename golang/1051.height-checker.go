ed52f34e7cb80102aee213a3f940d90484c1faf3
kevin chen <baimen.thinking@gmail.com>
2020-06-25 23:02:28 +0800

    feature: 1051.height checker

---
+++ golang/1051.height-checker.go
@@ -0,0 +1,81 @@
+import "sort"
+
+/*
+ * @lc app=leetcode id=1051 lang=golang
+ *
+ * [1051] Height Checker
+ *
+ * https://leetcode.com/problems/height-checker/description/
+ *
+ * algorithms
+ * Easy (70.59%)
+ * Likes:    215
+ * Dislikes: 1316
+ * Total Accepted:    59K
+ * Total Submissions: 83.5K
+ * Testcase Example:  '[1,1,4,2,1,3]'
+ *
+ * Students are asked to stand in non-decreasing order of heights for an annual
+ * photo.
+ *
+ * Return the minimum number of students that must move in order for all
+ * students to be standing in non-decreasing order of height.
+ *
+ * Notice that when a group of students is selected they can reorder in any
+ * possible way between themselves and the non selected students remain on
+ * their seats.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: heights = [1,1,4,2,1,3]
+ * Output: 3
+ * Explanation:
+ * Current array : [1,1,4,2,1,3]
+ * Target array  : [1,1,1,2,3,4]
+ * On index 2 (0-based) we have 4 vs 1 so we have to move this student.
+ * On index 4 (0-based) we have 1 vs 3 so we have to move this student.
+ * On index 5 (0-based) we have 3 vs 4 so we have to move this student.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: heights = [5,1,2,3,4]
+ * Output: 5
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: heights = [1,2,3,4,5]
+ * Output: 0
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
+ *
+ *
+ */
+// @lc code=start
+func heightChecker(heights []int) int {
+	sortArr := make([]int, len(heights), len(heights))
+	copy(sortArr[0:], heights[0:])
+	// 拷贝简便方法：sortArr := append([]int, heights...)
+	sort.Ints(sortArr)
+
+	count := 0
+	for i := 0; i < len(heights); i++ {
+		if sortArr[i] != heights[i] {
+			count++
+		}
+	}
+	return count
+}
+
+// @lc code=end
