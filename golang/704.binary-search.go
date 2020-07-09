5e53676dac64702218f38a100e4016537c5a09e9
kevin chen <baimen.thinking@gmail.com>
2020-07-09 22:56:29 +0800

    feature: 704.binary search

---
+++ golang/704.binary-search.go
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=704 lang=golang
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (51.56%)
+ * Likes:    675
+ * Dislikes: 44
+ * Total Accepted:    139.1K
+ * Total Submissions: 268.7K
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given a sorted (in ascending order) integer array nums of n elements and a
+ * target value, write a function to search target in nums. If target exists,
+ * then return its index, otherwise return -1.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ *
+ *
+ */
+
+// @lc code=start
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+// @lc code=end
