b7f5e63593ae64617caffce9a6cd252b145faaa6
kevin chen <baimen.thinking@gmail.com>
2020-07-13 22:52:36 +0800

    feature: 142.linked list cycle ii

---
+++ golang/142.linked-list-cycle-ii.go
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=142 lang=golang
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (36.63%)
+ * Likes:    2710
+ * Dislikes: 216
+ * Total Accepted:    323.8K
+ * Total Submissions: 876.8K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ * Note: Do not modify the linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow-up:
+ * Can you solve it without using extra space?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+
+	slow := head
+	fast := head
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	// 快慢指针相遇点，让其中一个指针从头开始，则再次相遇点为环起始点
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return fast
+}
+
+// @lc code=end
