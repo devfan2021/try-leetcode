a39a5be58c091f852fc29134e69d001e4d171e22
kevin chen <baimen.thinking@gmail.com>
2020-07-13 22:06:21 +0800

    feature:141.linked list cycle

---
+++ golang/141.linked-list-cycle.go
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (40.68%)
+ * Likes:    2890
+ * Dislikes: 480
+ * Total Accepted:    640.8K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	return hasCycle2(head)
+}
+
+// 采用hash方式
+func hasCycle2(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	hash := make(map[*ListNode]bool)
+	for head != nil {
+		if _, ok := hash[head]; ok {
+			return true
+		}
+		hash[head] = true
+	}
+	return false
+}
+
+// 采用快慢指针的方式
+func hasCycle1(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow := head
+	fast := head
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
