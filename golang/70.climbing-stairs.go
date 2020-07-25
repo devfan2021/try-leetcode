565bfce22fb22809ecde236e5f111aef18d2c189
kevin chen <baimen.thinking@gmail.com>
2020-07-25 22:49:36 +0800

    feature: 70.climbing stairs

---
+++ golang/70.climbing-stairs.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=70 lang=golang
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (46.95%)
+ * Likes:    4428
+ * Dislikes: 143
+ * Total Accepted:    696.9K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ *
+ * Example 1:
+ *
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= n <= 45
+ *
+ *
+ */
+
+// @lc code=start
+func climbStairs(n int) int {
+	return climbStairs2(n)
+}
+
+// solution by recursive with memorization
+func climbStairs2(n int) int {
+	cache := map[int]int{}
+	return helper(n, &cache)
+}
+
+func helper(n int, cache *map[int]int) int {
+	if v, ok := (*cache)[n]; ok {
+		return v
+	}
+	if n <= 2 {
+		(*cache)[n] = n
+		return n
+	}
+
+	retVal := helper(n-1, cache) + helper(n-2, cache)
+	(*cache)[n] = retVal
+	return retVal
+}
+
+// solution recursive, time limit exceeded
+func climbStairs1(n int) int {
+	if n <= 2 {
+		return n
+	}
+	return climbStairs(n-1) + climbStairs(n-2)
+}
+
+// @lc code=end
