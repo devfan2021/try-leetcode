3bb220b6464f3ce5d502cf045a74aee90244700b
kevin chen <baimen.thinking@gmail.com>
2020-08-05 16:37:46 +0800

    refactor:[golang].add new solution for 42.trapping rain water.

--- golang/42.trapping-rain-water.go
+++ golang/42.trapping-rain-water.go
@@ -32,59 +32,95 @@
 // @lc code=start
 // [4,2,3]
 func trap(height []int) int {
+	return trap3(height)
+}
+
+// using two pointer, time complexity: O(n), space complexity: O(n)
+func trap3(height []int) int {
+	left, right, count := 0, len(height)-1, 0
+	leftMax, rightMax := 0, 0
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] > leftMax {
+				leftMax = height[left]
+			} else {
+				count += leftMax - height[left]
+			}
+			left++
+		} else {
+			if height[right] > rightMax {
+				rightMax = height[right]
+			} else {
+				count += rightMax - height[right]
+			}
+			right--
+		}
+	}
+	return count
+}
+
+// this method similary brute method, enhance time complexity, using extra space to store max for each element
+// time complexity: O(n), space complexity: O(n)
+func trap2(height []int) int {
 	if len(height) <= 1 {
 		return 0
 	}
 
-	sum := 0
-	cmpVal := height[0]
-	for i := 0; i < len(height); {
-		calculate := false
-		if height[i] > cmpVal {
-			cmpVal = height[i]
-		}
-		if cmpVal > 0 {
-			calculate = true
-		}
-
-		if calculate {
-			nextI := i
-			for j := i + 1; j < len(height); j++ {
-				if height[j] >= cmpVal {
-					nextI = j
-					break
-				}
-			}
-			if nextI == i {
-				i++
-				if i >= len(height) {
-					break
-				}
-				cmpVal = height[i]
-				continue
-			}
-
-			minVal := cmpVal
-			if height[nextI] < minVal {
-				minVal = height[nextI]
-			}
-
-			for k := i; k <= nextI; k++ {
-				minus := minVal - height[k]
-				if minus > 0 {
-					sum += minus
-				}
-			}
-			// fmt.Println("=======")
-			// fmt.Println(sum)
-
-			cmpVal = height[nextI]
-			i = nextI
-		} else {
-			i++
-		}
+	leftMaxContainer := make([]int, len(height))
+	leftMaxVal := 0
+	for i := 0; i < len(height); i++ {
+		leftMaxVal = max(leftMaxVal, height[i])
+		leftMaxContainer[i] = leftMaxVal
 	}
-	return sum
+
+	rightMaxContainer := make([]int, len(height))
+	rightMaxVal := 0
+	for i := len(height) - 1; i >= 0; i-- {
+		rightMaxVal = max(rightMaxVal, height[i])
+		rightMaxContainer[i] = rightMaxVal
+	}
+
+	count := 0
+	for i := 1; i < len(height)-1; i++ {
+		leftMax, rightMax := leftMaxContainer[i], rightMaxContainer[i]
+		count += min(leftMax, rightMax) - height[i]
+	}
+	return count
+}
+
+// brute method, time complexity: O(n*n), space complexity: O(1)
+func trap1(height []int) int {
+	count, size := 0, len(height)
+	// start at index one and end at len(height) -2, not include first and last element
+	for i := 1; i < size-1; i++ {
+		maxLeft, maxRight := 0, 0
+		for j := i; j >= 0; j-- {
+			// search the left part for max bar size
+			maxLeft = max(maxLeft, height[j])
+		}
+
+		for j := i; j < size; j++ {
+			// search the right part for max bar size
+			maxRight = max(maxRight, height[j])
+		}
+
+		count += min(maxLeft, maxRight) - height[i]
+	}
+	return count
+}
+
+func max(val1, val2 int) int {
+	if val1 > val2 {
+		return val1
+	}
+	return val2
+}
+
+func min(val1, val2 int) int {
+	if val1 > val2 {
+		return val2
+	}
+	return val1
 }
 
 // @lc code=end
