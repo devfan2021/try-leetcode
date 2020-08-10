ff68b51dc27b4c4ec3bf89321a9b0c59ded41aca
kevin chen <baimen.thinking@gmail.com>
2020-08-10 18:22:46 +0800

    refactor:[golang].add new solution for 3.longest substring without repeating characters.

--- golang/3.longest-substring-without-repeating-characters.go
+++ golang/3.longest-substring-without-repeating-characters.go
@@ -52,9 +52,80 @@
 
 // @lc code=start
 // 测试例子：" ", "b", "bc", "dvdf", "tmmzuxt", "bbbbb"
-// 失败次数比较多，边界点点判断有问题, 及清空hash的逻辑
+// 失败次数比较多，边界点判断有问题, 及清空hash的逻辑
 // 待优化改进方法
 func lengthOfLongestSubstring(s string) int {
+	return lengthOfLongestSubstring4(s)
+}
+
+// brute force
+func lengthOfLongestSubstring4(s string) int {
+	if len(s) <= 1 {
+		return len(s)
+	}
+
+	begin, end := 0, 0
+	for end < len(s) {
+		if !isUniqueChars(&s, begin, end) {
+			begin++
+		}
+		end++
+	}
+	return end - begin
+}
+
+func isUniqueChars(s *string, begin, end int) bool {
+	for i := begin; i < end; i++ {
+		for j := i + 1; j <= end; j++ {
+			if (*s)[i] == (*s)[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// The reason is that if s[j] have a duplicate in the range [i, j) with index j' , we don't need to increase i little by little. We can skip all the elements in the range [i, j'] and let i to be j' + 1 directly.
+func lengthOfLongestSubstring3(s string) int {
+	hash, maxCount := make(map[byte]int), 0
+	for begin, end := 0, 0; end < len(s); end++ {
+		if index, ok := hash[s[end]]; ok { // check duplicate character, and skip to next index
+			if index > begin { // test case: tmmzuxt
+				begin = index
+			}
+		}
+		count := end - begin + 1
+		if count > maxCount {
+			maxCount = count
+		}
+		hash[s[end]] = end + 1
+	}
+	return maxCount
+}
+
+// sliding window, time complexity:O(n)
+func lengthOfLongestSubstring2(s string) int {
+	hash, length := make(map[byte]bool), len(s)
+	begin, end, maxCount := 0, 0, 0
+	for begin < length && end < length {
+		// c := int(s[end] - 'a')
+		c := s[end]
+		if hash[c] {
+			hash[s[begin]] = false
+			begin++
+		} else {
+			hash[c] = true
+			end++
+			count := end - begin
+			if count > maxCount {
+				maxCount = count
+			}
+		}
+	}
+	return maxCount
+}
+
+func lengthOfLongestSubstring1(s string) int {
 	count, begin := 0, 0
 	hash := make(map[string]int)
 	for i := 0; i < len(s); {
