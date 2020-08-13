a4ae474077e686c1c64d1bb6793df8f19354f962
kevin chen <baimen.thinking@gmail.com>
2020-08-13 16:22:21 +0800

    feature:[c].add c solution for 3.longest substring without repeating characters.

---
+++ algorithms/c/3.longest-substring-without-repeating-characters.c
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=3 lang=c
+ *
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (30.15%)
+ * Likes:    9426
+ * Dislikes: 567
+ * Total Accepted:    1.6M
+ * Total Submissions: 5.2M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * ⁠            Note that the answer must be a substring, "pwke" is a
+ * subsequence and not a substring.
+ *
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+extern inline int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int lengthOfLongestSubstring(char *s)
+{
+	int hash[128] = {0};
+	int maxCount = 0;
+	int begin = 0;
+	char ch;
+	for (int i = 0; (ch = s[i]) != 0; i++)
+	{
+		begin = max(hash[ch], begin);
+		maxCount = max(maxCount, i - begin + 1);
+		hash[ch] = i + 1;
+	}
+	return maxCount;
+}
+
+// @lc code=end
