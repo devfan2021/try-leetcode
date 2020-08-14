7e6871ebe0bfbb5f5b8c2481a8ea9d1f87d0f782
kevin chen <baimen.thinking@gmail.com>
2020-08-15 00:56:00 +0800

    feature:[golang].121.best time to buy and sell stock

---
+++ algorithms/golang/121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=121 lang=golang
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (50.17%)
+ * Likes:    5560
+ * Dislikes: 242
+ * Total Accepted:    920.9K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction (i.e., buy
+ * one and sell one share of the stock), design an algorithm to find the
+ * maximum profit.
+ *
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ *
+ *
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	return maxProfit2(prices)
+}
+
+func maxProfit2(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	minPrice, maxProfit := prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
+}
+
+// Brute Force
+func maxProfit1(prices []int) int {
+	maxValue := 0
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j]-prices[i] > maxValue {
+				maxValue = prices[j] - prices[i]
+			}
+		}
+	}
+	return maxValue
+}
+
+// @lc code=end
