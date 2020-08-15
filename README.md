dba92b7c71af953e4e5f48b9788a53a474b1ec3c
kevin chen <baimen.thinking@gmail.com>
2020-08-16 00:03:31 +0800

    feature:[MySQL].176.second highest salary

--- README.md
+++ README.md
@@ -33,9 +33,10 @@ LeetCode
 
 ### LeetCode Database
 
-| #   | Title                                                                   | Difficulty | Solution                                       |
-| --- | ----------------------------------------------------------------------- | ---------- | ---------------------------------------------- |
-| 175 | [Combine Two Tables](https://leetcode.com/problems/combine-two-tables/) | Easy       | [MySQL](./database/175.combine-two-tables.sql) |
+| #   | Title                                                                                     | Difficulty | Solution                                          |
+| --- | ----------------------------------------------------------------------------------------- | ---------- | ------------------------------------------------- |
+| 175 | [Combine Two Tables](https://leetcode.com/problems/combine-two-tables/)                   | Easy       | [MySQL](./database/175.combine-two-tables.sql)    |
+| 176 | [Second Highest Salary](https://leetcode.com/problems/second-highest-salary/description/) | Easy       | [MySQL](./database/176.second-highest-salary.sql) |
 
 
 reference:  
---
+++ database/176.second-highest-salary.sql
@@ -0,0 +1,50 @@
+--
+-- @lc app=leetcode id=176 lang=mysql
+--
+-- [176] Second Highest Salary
+--
+-- https://leetcode.com/problems/second-highest-salary/description/
+--
+-- database
+-- Easy (31.14%)
+-- Likes:    804
+-- Dislikes: 437
+-- Total Accepted:    237.4K
+-- Total Submissions: 750.8K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+--
+-- Write a SQL query to get the second highest salary from the Employee
+-- table.
+-- 
+-- 
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- 
+-- 
+-- For example, given the above Employee table, the query should return 200 as
+-- the second highest salary. If there is no second highest salary, then the
+-- query should return null.
+-- 
+-- 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- 
+-- 
+--
+
+-- @lc code=start
+-- Write your MySQL query statement below
+SELECT IFNULL((SELECT DISTINCT Salary
+  FROM Employee
+  order by Salary DESC LIMIT 1 OFFSET
+1), NULL) AS SecondHighestSalary
+
+-- @lc code=end
