d807156d180fd1e76a9aae4058f3dfc92c87c7ab
kevin chen <baimen.thinking@gmail.com>
2020-08-23 23:29:43 +0800

    feature:[c].offer.65-add two numbers.

---
+++ lcof/offer-65-AddTwoNumbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * ^: 0^0=0, 1^1=0, 0^1=1, 1^0=1
+ * &: 0&0=0, 1&0=0, 0&1=0, 1&1=1
+ */
+int add(int a, int b)
+{
+  int sum = 0, carry = 0;
+  do
+  {
+    sum = a ^ b;
+    carry = (a & b) << 1;
+
+    a = sum;
+    b = carry;
+
+  } while (carry != 0);
+  return a;
+}
+
+int main()
+{
+  printf("==%d\n", add(0, 0));
+  printf("==%d\n", add(0, 1));
+  printf("==%d\n", add(1, 1));
+  printf("==%d\n", add(1, 0));
+  printf("==%d\n", add(2, 3));
+  return 0;
+}
