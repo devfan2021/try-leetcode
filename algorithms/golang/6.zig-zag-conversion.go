2e8fcc631fde3894a19e7110565049a209d46e19
kevin chen <baimen.thinking@gmail.com>
2020-08-25 17:08:15 +0800

    refactor:[golang].update 6.zig zag conversion.

--- algorithms/golang/6.zig-zag-conversion.go
+++ algorithms/golang/6.zig-zag-conversion.go
@@ -64,6 +64,15 @@ func convert(s string, numRows int) string {
 // line 0: indexK = 2 * numRows -2
 // numRows -1, indexK = k(2*numRows-2) + numRows -1
 // line i: k, 2 * numRows -2 + i  or  (k+1)(2*numRows-2)
+/*n=numRows
+Δ=2n-2    1                           2n-1                         4n-3
+Δ=        2                     2n-2  2n                    4n-4   4n-2
+Δ=        3               2n-3        2n+1              4n-5       .
+Δ=        .           .               .               .            .
+Δ=        .       n+2                 .           3n               .
+Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+Δ=2n-2    n                           3n-2                         5n-4
+*/
 func convert2(s string, numRows int) string {
 	if len(s) <= numRows || numRows == 1 {
 		return s
